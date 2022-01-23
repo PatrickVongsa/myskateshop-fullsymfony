b95216558f53c5ce1dee3af213e6df014c9cade1
PatrickVongsa <patrick.vongsa@gmail.com>
2022-01-23 17:00:23 +0100

    type form done

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
@@ -13,13 +18,13 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
-            ->add('size')
-            ->add('stock')
-            ->add('minStock')
-            ->add('isVisible')
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('price', NumberType::class)
+            ->add('size', NumberType::class)
+            ->add('stock', IntegerType::class)
+            ->add('minStock', IntegerType::class)
+            ->add('isVisible', CheckboxType::class)
             ->add('category', null, [
                 'choice_label' => 'category',
                 'label' => 'Catégorie'
